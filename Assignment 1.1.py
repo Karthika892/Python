540e61e82d05217d38c94f9bd6fb1f53a2d3ff67
Karthika892 <98149827+Karthika892@users.noreply.github.com>
2022-01-26 15:47:53 +0530

    Add files via upload

---
+++ Assignment 1.1.py
@@ -0,0 +1,122 @@
+#Assignment 1.1
+#1 Comparison and Assignment Operators
+
+#a)
+a=5
+b=8
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+ 
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a%=b
+print(a)
+a//=b
+print(a)
+a**=b
+print(a)
+
+#b
+c=4
+d=-1
+print(c==d)
+print(c!=d)
+print(c>d)
+print(c<d)
+print(c<=d)
+print(c>=d)
+
+c+=d
+print(c)
+c-=d
+print(c)
+c*=d
+print(c)
+c/=d
+print(c)
+c%=d
+print(c)
+c//=d
+print(c)
+c**=d
+print(c)
+
+#c
+e=3
+f=2
+print(e==f)
+print(e!=f)
+print(e>f)
+print(e<f)
+print(e<=f)
+print(e>=f)
+
+
+e+=f
+print(e)
+e-=f
+print(e)
+e*=f
+print(e)
+e/=f
+print(e)
+e%=f
+print(e)
+e//=f
+print(e)
+e**=f
+print(e)
+
+#d
+g=9
+h=-5
+print(g==h)
+print(g!=h)
+print(g>h)
+print(g<h)
+print(g<=h)
+print(g>=h)
+
+g+=h
+print(g)
+g-=h
+print(g)
+g*=h
+print(g)
+g/=h
+print(g)
+g%=h
+print(g)
+g//=h
+print(g)
+g**=h
+print(g)
+
+#2 If and else statement
+
+#a If it snows I will go out else I will not
+snow=0
+if snow==1:
+    print("i will go out")
+else:
+    print("i will not")
+
+
+#b If it rains frogs I will dance 
+rain=0
+if rain==1:
+    print("i will dance")
+else:
+    print("i will not dance")
+
+
